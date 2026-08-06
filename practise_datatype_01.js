d2a1efcf5dfb1bfee9bea610ade1df1fd4e5eb3b
prahisuja-creator <prahisuja@gmail.com>
2026-08-06 13:17:49 +0530

    Add files via upload

---
+++ practise_datatype_01.js
@@ -0,0 +1,21 @@
+const { type } = require("node:os");
+
+let firstname ='Prahina'
+console.log("Firstname",firstname);
+console.log(typeof firstname);
+
+let companyname ='TechM'
+console.log("Company name", companyname);
+console.log(typeof companyname);
+
+let mobilenumber = 9999123499
+console.log("Mobile number", mobilenumber);
+console.log(typeof mobilenumber);
+
+let hasPlaywright 
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright)
+
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
