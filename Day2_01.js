9afaa2cd3d8ea85c39ebb32490d36ed87186e92b
prahisuja-creator <prahisuja@gmail.com>
2026-08-10 10:06:16 +0530

    Add files via upload

---
+++ Day2_01.js
@@ -0,0 +1,13 @@
+
+const browserVersion = "chrome"
+
+function  getBrowserVersion()
+{
+    if (browserversion ="chrome")
+        {
+            let browserVersion="chromeV6"
+            console.log("call inside the block", browserVersion);
+        }
+        console.log("call inside the function", browserVersion);
+}
+getBrowserVersion() 
