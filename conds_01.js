c2a9c311b9cc69cf51f3c7736c38c5f9a17d57fb
prahisuja-creator <prahisuja@gmail.com>
2026-08-11 22:17:00 +0530

    Add files via upload

---
+++ conds_01.js
@@ -0,0 +1,17 @@
+
+//Create a JavaScript function that determines if a number is positive, negative, or zero and returns a 
+//corresponding string indicating the type. 
+
+function numberFind() {
+    var num = 5
+    if (num > 0) {
+        console.log("It is a positive number");
+    }
+    else if (num < 0) {
+console.log("It is negative number");
+    }
+    else {
+        console.log("It is neutral");
+    }
+}
+numberFind();
